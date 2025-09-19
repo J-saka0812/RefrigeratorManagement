4e41e7edbbb4a8fb868bb2ed5e1cab68430dae8e
J-saka0812 <j.saka0812@gmail.com>
2025-09-19 21:58:22 +0900

    🎉 springboot環境構築

---
+++ RefrigeratorManagement.backend/src/main/java/single/project/refrigerator_app/RefrigeratorAppApplication.java
@@ -0,0 +1,13 @@
+package single.project.refrigerator_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefrigeratorAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefrigeratorAppApplication.class, args);
+	}
+
+}
