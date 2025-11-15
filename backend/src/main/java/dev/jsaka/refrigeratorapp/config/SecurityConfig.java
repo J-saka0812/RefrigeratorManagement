d9bb5ae0927a5a0d6508d1e4fd328bb906229a85
J-saka0812 <j.saka0812@gmail.com>
2025-11-15 20:34:08 +0900

    🐛CSRF無効化

--- backend/src/main/java/dev/jsaka/refrigeratorapp/config/SecurityConfig.java
+++ backend/src/main/java/dev/jsaka/refrigeratorapp/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package dev.jsaka.refrigeratorapp.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,8 +11,6 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -23,6 +23,7 @@ public class SecurityConfig {
                 // それを許可するためのルールを追加する必要がある
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(AbstractHttpConfigurer::disable)
+                .csrf(csrf -> csrf.disable()) // CSRF保護を無効化する
 
                 // 認証フィルターの許可ルールなどを記述
                 .authorizeHttpRequests(authorize -> authorize
